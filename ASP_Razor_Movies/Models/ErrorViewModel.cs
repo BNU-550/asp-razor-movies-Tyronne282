101d5c5018f8a46a4f1d18e3e94dce5d15b12292
Tyronne282 <bradburntyronne@gmail.com>
2021-10-27 15:51:40 +0100

    Add project files.

---
+++ ASP_Razor_Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_Razor_Movies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
