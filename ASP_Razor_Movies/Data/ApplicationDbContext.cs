9d56b4c29aaad8deec32793a799a1c97abb094a3
Tyronne282 <bradburntyronne@gmail.com>
2021-10-27 16:20:49 +0100

    Added code to name the database

--- ASP_Razor_Movies/Data/ApplicationDbContext.cs
+++ ASP_Razor_Movies/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ASP_Razor_Movies.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace ASP_Razor_Movies.Data
             : base(options)
         {
         }
+
+        public DbSet<Movie> Movies; 
     }
 }
